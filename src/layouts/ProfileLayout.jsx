2056318f3a03dc1c09e210ba13df2a352f5e84b0
Bintang Bagus Pangestu <bintangbgs16@gmail.com>
2025-02-04 21:06:03 +0700

    working on responsive table in order profiile

--- src/layouts/ProfileLayout.jsx
+++ src/layouts/ProfileLayout.jsx
@@ -5,15 +5,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import { userLogout } from '../app/features/userSlice';
 import { removeWhenLogout } from '../app/features/deliveryAddressSlice';
 import { toast } from 'react-toastify';
-import { FaChevronRight } from 'react-icons/fa';
+import { FaChevronRight, FaChevronDown } from 'react-icons/fa';
 import { TbLogout } from 'react-icons/tb';
 import { clearCart } from '../app/features/cartSlice';
 import './ProfileLayout.scss';
+import { useEffect, useState } from 'react';
 
 const ProfileLayout = () => {
   const { id } = useSelector((state) => state.user);
+  const [width, setWidth] = useState(window.innerWidth);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [width]);
 
   const handleLogout = () => {
     toast.error('Logout berhasil!', {
@@ -32,26 +41,38 @@ const ProfileLayout = () => {
   return (
     <div className="profile-page">
       <img src={defaultAvatar} className="avatar" />
-      <h2>Account ID : {id}</h2>
+      <h2>
+        Account ID : <span>{id}</span>
+      </h2>
+      {width < 551 && (
+        <button className="logout-btn" onClick={handleLogout}>
+          <p>Logout</p>
+          <TbLogout size={24} />
+        </button>
+      )}
+
       <div className="profile-details">
         <div className="side-bar">
           <NavLink className="profile-link" to="/user/profile">
             <p>Profil</p>
-            <FaChevronRight />
+            {width > 550 ? <FaChevronRight /> : <FaChevronDown />}
           </NavLink>
           <NavLink className="profile-link" to="/user/order">
             <p>Pemesanan</p>
-            <FaChevronRight />
+            {width > 550 ? <FaChevronRight /> : <FaChevronDown />}
           </NavLink>
           <NavLink className="profile-link" to="/user/address">
             <p>Alamat</p>
-            <FaChevronRight />
+            {width > 550 ? <FaChevronRight /> : <FaChevronDown />}
           </NavLink>
-          <button className="logout-btn" onClick={handleLogout}>
-            <p>Logout</p>
-            <TbLogout size={24} />
-          </button>
+          {width > 550 && (
+            <button className="logout-btn" onClick={handleLogout}>
+              <p>Logout</p>
+              <TbLogout size={24} />
+            </button>
+          )}
         </div>
+
         <div className="expanded">
           <Outlet />
         </div>
